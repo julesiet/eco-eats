40b27236ded5c8a895ceb3d9dc316c8ba0bd28a0
inkiput <danicajadenm@gmail.com>
2026-03-21 15:46:22 -0700

    wow dan's computer hates everyone

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
